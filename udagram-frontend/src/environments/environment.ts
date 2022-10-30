882721761874d5e24f666901f70412610d7d5757
AlgyJr <algymussa@gmail.com>
2022-10-30 20:17:24 +0200

    :recycle: refactor: set frontend gateway

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://aa82ed455ff124c84b9311ea459c0ffe-2011041066.us-east-1.elb.amazonaws.com:8080/api/v0'
+  apiHost: 'http://a57dc39d64d234a999dbfba2ce131151-1871390205.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
